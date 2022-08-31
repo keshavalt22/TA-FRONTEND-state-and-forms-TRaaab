37d5257adb0dd3e17aaf6b101b1aefbab501b66e
keshavalt22 <keshavkhatri35@gmail.com>
2022-08-31 12:27:55 +0530

    assignment of shipping and billing address

---
+++ block-BRaabs/billing-app/src/components/App.js
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import Shipping from "./Shipping";
+
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Shipping/>
+    </>
+  );
+}
+
+export default App;
